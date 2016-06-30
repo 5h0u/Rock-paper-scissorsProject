e66eb9fb446876afa63130b5d85d7288016c8a44
山崎祥太郎 <shotaro@yamazakishoutarou-no-MacBook-Air.local>
2016-06-30 20:52:33 +0900

    Viewをxibに変更

---
+++ RockpaperscissorsProject/RockpaperscissorsProject/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  RockpaperscissorsProject
+//
+//  Created by 山崎祥太郎 on 2016/06/29.
+//  Copyright © 2016年 山崎祥太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
+
