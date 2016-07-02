de30c61bb9acc4ecec2ed8612ae4c75cfe4de1b1
山崎祥太郎 <shotaro@yamazakishoutarou-no-MacBook-Air.local>
2016-07-02 23:46:03 +0900

    GameViewの作成

---
+++ RockpaperscissorsProject/RockpaperscissorsProject/GameView.h
@@ -0,0 +1,13 @@
+//
+//  GameView.h
+//  RockpaperscissorsProject
+//
+//  Created by 山崎祥太郎 on 2016/07/02.
+//  Copyright © 2016年 山崎祥太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameView : UIView
+
+@end
