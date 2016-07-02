08e7d0778b23e06e741ce136b817da5b502b317a
山崎祥太郎 <shotaro@yamazakishoutarou-no-MacBook-Air.local>
2016-07-03 00:19:18 +0900

    ボタンにアクションをセット

--- RockpaperscissorsProject/RockpaperscissorsProject/GameView.h
+++ RockpaperscissorsProject/RockpaperscissorsProject/GameView.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface GameView : UIView
+
+
 
 @end
--- RockpaperscissorsProject/RockpaperscissorsProject/GameView.m
+++ RockpaperscissorsProject/RockpaperscissorsProject/GameView.m
@@ -8,7 +8,38 @@
 
 #import "GameView.h"
 
+@interface GameView()
+
+@property (weak, nonatomic)IBOutlet UIButton *rockButton;
+@property (weak, nonatomic)IBOutlet UIButton *paperButton;
+@property (weak, nonatomic)IBOutlet UIButton *scissorsButton;
+@property (weak, nonatomic)IBOutlet UILabel *serifLabel;
+
+@end
+
 @implementation GameView
+
+-(id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if(self){
+        [_rockButton addTarget:self action:@selector(pressRockButton) forControlEvents:UIControlEventTouchDown];
+        [_paperButton addTarget:self action:@selector(pressPaperButton) forControlEvents:UIControlEventTouchDown];
+        [_scissorsButton addTarget:self action:@selector(pressScissorsButton) forControlEvents:UIControlEventTouchDown];
+    }
+    return self;
+}
+
+-(void)pressRockButton{
+    
+}
+
+-(void)pressPaperButton{
+    
+}
+
+-(void)pressScissorsButton{
+    
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
